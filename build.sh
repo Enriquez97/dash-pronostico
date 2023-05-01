3ba6304896f634dba3f8ac852c5fc7cf4f55fda2
Enriquez97 <pierenriquez97@gmail.com>
2023-05-01 12:45:42 -0500

    change require2

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install --upgrade pip
 #poetry install
 #pip install -r requirements.txt
+python -m pip install --upgrade pip
 python manage.py collectstatic --no-input
 python manage.py migrate
